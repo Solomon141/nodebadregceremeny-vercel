c68fca7e675f09046c9ada35244f7bf9dbfacdc1
Solomon141 <31657654+Solomon141@users.noreply.github.com>
2023-05-02 06:21:49 +0300

    first commi newt

--- Conn.js
+++ Conn.js
@@ -2,16 +2,10 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const con = mysql.createConnection({
-  // host: "localhost",
-  // user: "badregplccom_newweb",
-  // password: "P@55w0rdP@55w0rd",
-  // database: "badregplccom_newweb",
-
   HOST: process.env.HOST,
   USER: process.env.USER,
   DB_PASSWORD: process.env.DB_PASSWORD,
   DATABASE: process.env.DATABASE,
-
 });
 
 con.connect(function (err) {
